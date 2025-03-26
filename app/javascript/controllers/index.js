495eaba52ffc15b01c2d85b8a2068120113a402f
Albert Nikolli <albert_nikolli@icloud.com>
2025-03-26 17:23:08 +0100

    added again some lines out of the dashboard controlloer

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,6 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"£
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
 // import DashboardController from "./dashboard_controller"
 eagerLoadControllersFrom("controllers", application)
 
