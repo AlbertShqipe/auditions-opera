5a3e58b90f752dafe12c53758bb3e945791ad137
Albert Nikolli <albert_nikolli@icloud.com>
2025-03-26 18:27:18 +0100

    added hispanic to the ethnicity

--- app/models/ethnicity.rb
+++ app/models/ethnicity.rb
@@ -2,5 +2,5 @@ class Ethnicity < ApplicationRecord
   has_many :audition_applications
 
   # Validate that the name is included in the allowed list and is unique
-  validates :name, presence: true, uniqueness: true, inclusion: { in: ["Caucasian", "Asian", "Black", "Other"] }
+  validates :name, presence: true, uniqueness: true, inclusion: { in: ["Caucasian", "Asian", "Black", "Hispanic", "Other"] }
 end
